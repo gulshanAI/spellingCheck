a17c8d0ea148f38ac22e5d320a00f4d8247b4cf4
gulshanai <gulshanprajapati5105@gmail.com>
2021-06-26 13:21:29 +0530

    updated

--- checkSpelling/settings.py
+++ checkSpelling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-myuku2mzincpj3dr3)xau03#uwbtj184hjpw%xd58riq-$zgw(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
